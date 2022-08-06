8a906c7d57a2bb22ca7b9815e51f9a5f6b86c29f
XNX-XNX <104139082+XNX-XNX@users.noreply.github.com>
2022-08-06 09:47:04 +0600

    Update lite.py

--- lite.py
+++ lite.py
@@ -1,12 +1,4 @@
 import platform,os
 os.system('git pull')
 os.system('clear')
-
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("litex").login()
-elif 'aarch' in arc:
-	__import__("lite").login()
-else:
-	__import__("litexx").login()
+os.system('python litex.py')
